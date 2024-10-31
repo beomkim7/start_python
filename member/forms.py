57fc47aee4baf73dbc1febb804b57b7a42dbc1ce
beomkim7 <beomkim7@naver.com>
2024-10-31 14:30:36 +0900

    signin

--- member/forms.py
+++ member/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 
-class UserSignupForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-
+class UserSignupForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username','password','email']
+        fields = ['username','password1','password2','email']
 
-class UserSigninForm(AuthenticationForm):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(UserSignupForm, self).__init__(*args, **kwargs)
+        self.fields['username'].label = '아이디'  # username 필드의 레이블을 '아이디'로 변경
+        self.fields['password1'].label = '비밀번호'  # password1 필드 레이블 변경
+        self.fields['password2'].label = '비밀번호 확인'  # password2 필드 레이블 변경
--- member/views.py
+++ member/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth import login,authenticate
 from django.contrib.auth.models import User
 from .forms import UserSignupForm
@@ -6,13 +7,20 @@ from .forms import UserSignupForm
 # Create your views here.
 
 def Signup(request):
-    if request.method == 'post':
-        form = UserSignupForm(request.post)        
+    if request.method == 'POST':
+        form = UserSignupForm(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            return redirect('Singup')
+            form.save()
+            id = form.cleaned_data.get('username')
+            pw = form.cleaned_data.get('password1')
+            user = authenticate(username=id,password=pw)
+            login(request,user)
+            return redirect('board:board_list')
+        else:
+            for fields, errors in form.errors.items():
+                label = form.fields[fields].label
+                for error in errors:
+                    messages.error(request, f"{label}: {error}")
     else:
         form = UserSignupForm()
-        return render(request,'member/signup.html',{'form':form})
+    return render(request,'member/signup.html',{'form':form})
