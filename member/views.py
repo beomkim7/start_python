b7cacd49cda3782b7216bc0900dd10526af25c17
beomkim7 <beomkim7@naver.com>
2024-10-31 16:16:50 +0900

    signin

--- member/views.py
+++ member/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
-from .forms import UserSignupForm
+from .forms import UserSignupForm, UserSigninForm
 
 # Create your views here.
 
@@ -31,18 +31,23 @@ def Signout(request):
 
 def Signin(request):
     if request.method == 'POST':
-        form = UserSignupForm(request.POST)
-        print(request)
+        form = UserSigninForm(request, data=request.POST)
         if form.is_valid():
-            id = form.cleaned_data.get('username')
-            pw = form.cleaned_data.get('password')
-            user = authenticate(username=id,password=pw)
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
             if user is not None:
-                login(request,user)
-                messages(request,"로그인 성공!")
+                login(request, user)
+                messages.success(request, "로그인 성공!")
                 return redirect('board:board_list')
             else:
-                messages(request,"아이디,비번확인")
+                messages.error(request, "아이디 또는 비밀번호를 확인하세요.")
+        else:
+            # 폼 유효성 검사에서 오류가 발생한 경우
+            print(form.errors)  # 오류를 출력
+            messages.error(request, "아이디 또는 비밀번호를 확인하세요1.")
+            return redirect('member:signin')
     else:
-        form = UserSignupForm()
-    return render(request,'member/signin',{'form':form})
+        form = UserSigninForm()
+
+    return render(request, 'member/signin.html', {'form': form})
